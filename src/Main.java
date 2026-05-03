2c44496b51459c6e9fdfe1c124409832d1f06d0e
TANAN <tanan@TANANs-MacBook-Air.local>
2026-05-03 22:27:33 +0300

    update1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.lang.reflect.Field;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class vehicle{
+    final void action(){
+        System.out.println("vehicle moving");
     }
+}
+class audi extends vehicle {
+    int doors =4;
+    int whiles =4;
+
+    String material;
+    void honk(){
+        System.out.println("Honks loud");
+    }
+}
+class Types extends audi {
+    String type;
+
+   Types(String type,String material){
+       this.type=type;
+       super.material=material;
+   }
+}
+class BMW extends vehicle{
+    void honk(){
+        System.out.println("Honks genteelly");
+    }
+}
+final class specifics extends Types {
+    String color ;
+    int size ;
+    specifics(String color,int size){
+        this.color=color;
+        this.size=size;
+    }
+}
+class main{
+    public static void main(String[] args){
+
+    }
+
 }
